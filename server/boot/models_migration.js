d1fec10c55c44045a738888a57d7a9c5ef556968
larissasilabi <larissa@softclass.com.br>
2018-05-28 22:00:57 -0300

    Add files via upload

---
+++ server/boot/models_migration.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = function updateCustomModels(app, next) {
+
+    // Do not run when server was started via migrate.js
+    if (require.main.filename.endsWith('migrate.js')) {
+        return next();
+    }
+
+    const models = ['acao', 'log-acao', 'log-sensor', 'parametro', 'sensor',
+        'usuario'];
+
+    // reference to our datasource
+    const postgres = app.dataSources.postgres;
+
+    // check to see if the model is out of sync with DB
+    postgres.isActual(models, (err, actual) => {
+
+        if (err) {
+            throw err;
+        }
+
+        let syncStatus = actual ? 'in sync' : 'out of sync';
+
+        console.log(`Models are ${syncStatus}`);
+
+        if (actual) return next();
+
+        console.log('Migrating Models...');
+
+        postgres.autoupdate(models, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            console.log('Models migration successful!');
+            next();
+        });
+
+    });
+
+};
